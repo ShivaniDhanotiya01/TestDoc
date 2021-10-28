5de5eef337966f91d83c55f5272a6caaf09f613f
Shivani Dhanotiya <shivani.dhanotiya@cognizant.com>
2021-10-28 12:03:06 +0530

    first commit

---
+++ OrdersData.kt
@@ -0,0 +1,7 @@
+package com.example.bakersvilla.homescreen.orders.api.model
+
+import com.example.bakersvilla.homescreen.orders.api.model.Orders
+
+data class OrdersData(
+    val data: List<Orders>
+)
